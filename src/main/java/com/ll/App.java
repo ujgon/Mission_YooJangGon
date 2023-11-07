d17d3a53f89b6b32d58625addd4c4c08b0dcb769
Yoo <ujgon@naver.com>
2023-11-08 03:32:11 +0900

    test

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,28 @@
+package com.ll;
+
+import java.util.Scanner;
+
+class App {
+    void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+            }
+        }
+    }
+}
