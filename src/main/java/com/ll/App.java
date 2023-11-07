fa4b32837bfd12f7e7881f0a18200ea5c7633a11
Yoo <ujgon@naver.com>
2023-11-08 05:04:25 +0900

    5단계

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,10 +1,15 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
+
+        int lastQuotationId = 0;
+        List<Quotation> quotations = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -21,7 +26,29 @@ class App {
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
 
-                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+                lastQuotationId++;
+                int id = lastQuotationId;
+
+                Quotation quotation = new Quotation(id, content, authorName);
+                quotations.add(quotation);
+
+                System.out.println(lastQuotationId + "번 명언이 등록되었습니다.");
+            } else if (cmd.equals("목록")) {
+
+
+                System.out.println("번호 / 작가 / 명언");
+
+                System.out.println("------------------");
+
+                if (quotations.isEmpty())
+                    System.out.println("등록된 명언이 없습니다.");
+
+                for (int i = quotations.size() -1; i >= 0; i--) {
+
+                    Quotation quotation = quotations.get(1);
+                    System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+
+                }
             }
         }
     }
---
+++ src/main/java/com/ll/Quotation.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Quotation {
+    int id;
+    String content;
+    String authorName;
+
+    public Quotation(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
